1072e56646fcf606203e812fde7eac25bb3476db
shmuel vazana <shmuelha@g.jct.ac.il>
2020-10-05 19:43:04 +0300

    First lesson

---
+++ DrugSystem/BLL.BE/AuxiliaryObjects.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.BE
+{
+    class AuxiliaryObjects
+    {
+        public enum Gender { Male, Feamle, Other }
+        public struct Address
+        {
+            public string City { get; set; }
+            public string Street { get; set; }
+            public string BuildingNumber { get; set; }
+        }
+    }
+}
---
+++ DrugSystem/BLL.BE/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.BE
+{
+    class Person
+    {
+        public string ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string BirthDay { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public string Gender { get; set; }
+        public string FatherName { get; set; }
+    }
+}
