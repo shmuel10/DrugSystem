101af180fda86afd45442c2d69971f6743e3158f
SimchaPo <45201254+SimchaPo@users.noreply.github.com>
2020-10-05 20:27:53 +0300

    add some entuty classes

---
+++ DrugSystem/BLL.BE/Medicine.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.BE
+{
+    class Medicine
+    {
+        public string CommercialName { get; set; }
+        public string GenericName { get; set; }
+        public string Manufacturer { get; set; }
+        public List<string> ActiveIngredients { get; set; }
+        public List<string> DoseCharacteristics { get; set; }
+        public Uri ImageUrl { get; set; }
+    }
+}
---
+++ DrugSystem/BLL.BE/prescription.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.BE
+{
+    class Prescription
+    {
+        public Patient PatientDetailes { get; set; }
+        public Medicine MedicineDetailes { get; set; }
+
+        public Doctor DoctorDetailes { get; set; }
+
+
+    }
+}
