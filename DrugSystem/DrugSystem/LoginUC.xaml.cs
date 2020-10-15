7920cc61d4ad4c018ae760b3068341774ed924cd
SimchaPo <45201254+SimchaPo@users.noreply.github.com>
2020-10-15 14:04:31 +0300

    validate email address

--- DrugSystem/DrugSystem/LoginUC.xaml.cs
+++ DrugSystem/DrugSystem/LoginUC.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -32,7 +33,19 @@ namespace DrugSystem
 
         private void SignInButton_Click(object sender, RoutedEventArgs e)
         {
-            if (UserMail.Text.Length != 0 && Userpassword.Text == "123")
+            try
+            {
+                Validations.ValidateEmailAddress(UserMail.Text);
+            } catch(FormatException)
+            {
+                MessageBox.Show("invalid email address!", "ERROR!", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            catch (ArgumentException)
+            {
+                MessageBox.Show("missing email address!", "ERROR!", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+            if (Userpassword.Text == "123")
             {
                 if (ClickHandler != null)
                 {
